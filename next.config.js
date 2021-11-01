cc78ef43cd3e434e7df6c2ae40779d6fc2dd89d0
Shen Cong <congkevin@gmail.com>
2021-10-31 23:14:16 -0400

    optimized bg png image

--- next.config.js
+++ next.config.js
@@ -5,10 +5,18 @@ const optimizedImages = require('next-optimized-images');
 module.exports = withPlugins([
   [optimizedImages, {
     /* config for next-optimized-images */
-    images: {
-      handleImages: ['jpg'],
-      disableStaticImages: true
-    }
+    handleImages: ['jpg', 'png'],
+    disableStaticImages: true,
+    optimizeImages: true,
+    optipng: {
+      optimizationLevel: 3,
+    },
+    pngquant: false,
+    webp: {
+      preset: 'default',
+      quality: 75,
+    },
   }],
+  
   // your other plugins here
 ]);
